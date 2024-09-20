8a88f856cc32c9bae27bd20f0bdae0becb6b0bc5
The-R0nin <2025esteele@isd2144.org>
2024-09-19 21:38:58 -0500

    Update program_3.py
    got the first 3 to work by changing them to if's  but the the greater than 10 keeps coming out as none. Can you help?
    
    Ethan Steele
    9/19

--- program_3.py
+++ program_3.py
@@ -4,16 +4,19 @@ def weight_conversion(weight):
     
     if weight <= 2:
          shippingCost = 1.50
-    elif weight > 2 and weight < 6:
+
+    if weight > 2 and weight < 6:
          shippingCost = 3.00
-    elif weight >= 6 and weight < 10:
+
+    if weight >= 6 and weight < 10:
          shippingCost = 4.00
+
     elif weight >= 10:
          shippingCost = 4.75
-    else:
-         shippingCost = 0.0
 
-    
+    else:
+         shippingCost == 0.0
+
          return shippingCost
 shippingCost = weight_conversion(float(weight))
 print ('Shipping charge: $', shippingCost)
