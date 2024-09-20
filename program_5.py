b671567f53a881561f66066f92e5af0d7838bdd9
The-R0nin <2025esteele@isd2144.org>
2024-09-20 15:43:43 -0500

    Update program_5.py
    I couldn't figure it out
    Ethan Steele
    9/20

--- program_5.py
+++ program_5.py
@@ -1,22 +1,30 @@
-hotdogs = input(str("Enter hotdog: "))
-Hotdog = 3.50
-Chilidog = 4.50
-if hotdogs == "hotdog" or "chilidog":
-    "hotdog" = Hotdog
+
+Order = input(str("Enter hotdog: "))
+#prices for the hotdogs variables \/
+Hotdog = int(3.50)
+
+Chilidog = int(4.50)
+#I am trying to trun the answer into a variable \/
+if Order == "hotdog" or "chilidog":
+    "hotdog" == Hotdog
     "chilidog" ==  Chilidog
     toppings = print(input(str("Enter topping: ")))
+#prices for the toppings variables\/
+Cheese = int(0.50)
 
-Cheese = 0.50
-Peppers = 0.75
-Grilled_Onions = 1.00
-none = 0.00
+Peppers = int(0.75)
+
+Grilled_Onions = int(1.00)
+
+none = int(0.0)
+#I am trying to trun the answer into a variable \/
 if toppings == 'cheese' or 'peppers' or 'grilled onions':
     'cheese' == Cheese
     'peppers' == Peppers
     'grilled onions' == Grilled_Onions
 
-total_price = float(input((hotdogs + toppings) * 0.07))
+total_price = float(input((int(Order) + int(toppings)) * 0.07))
 
-print("Hotdog cost is: ", hotdogs)
+print("Hotdog cost is: ", Order)
 print("The taxt is: 7%")
 print("Your total is: ", total_price)
