6543d0714fccfd97e585e77e2c1e973819b7ccfb
The-R0nin <2025esteele@isd2144.org>
2024-09-19 21:55:11 -0500

    Update program_5.py
    Can't figure out why they won't get inputted into the equations. Could you check it and give a little guidance on what I can do to fix it?
    Ethan Steele
    9/19

--- program_5.py
+++ program_5.py
@@ -1,6 +1,22 @@
-# There are two kinds of hot dogs sold:  
-# Hot Dog ($3.50), Chili Dog ($4.50).  
-# Additionally a person can order cheese ($0.50), peppers ($0.75), or grilled onions ($1.00).  
-# Also tax is 7%. 
-# Write a program the inputs the type of hot dog wanted and optional toppings.  
-# The program then displays the hot dog cost, tax and total cost. 
+hotdogs = input(str("Enter hotdog: "))
+Hotdog = 3.50
+Chilidog = 4.50
+if hotdogs == "hotdog" or "chilidog":
+    "hotdog" = Hotdog
+    "chilidog" ==  Chilidog
+    toppings = print(input(str("Enter topping: ")))
+
+Cheese = 0.50
+Peppers = 0.75
+Grilled_Onions = 1.00
+none = 0.00
+if toppings == 'cheese' or 'peppers' or 'grilled onions':
+    'cheese' == Cheese
+    'peppers' == Peppers
+    'grilled onions' == Grilled_Onions
+
+total_price = float(input((hotdogs + toppings) * 0.07))
+
+print("Hotdog cost is: ", hotdogs)
+print("The taxt is: 7%")
+print("Your total is: ", total_price)
