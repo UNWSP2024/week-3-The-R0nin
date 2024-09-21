745ce94ab6ed7017f0d83b4bce8dc113137a293c
The-R0nin <2025esteele@isd2144.org>
2024-09-20 21:53:58 -0500

    Update program_5.py
    Ethan Steele
    9/19
    changed the code

--- program_5.py
+++ program_5.py
@@ -1,3 +1,4 @@
+
 Order = input(str("Enter hotdog: "))
 Order_Price = 0.0
 if Order == "hotdog":
@@ -6,19 +7,19 @@ if Order == "hotdog":
 
 elif Order == "chilidog":
     Order_Price = 4.50
-    toppings = print(input(str("Enter topping: ")))
 
-    if toppings == "cheese":
-        toppings_price = int(0.50)
+toppings = print(input(str("Enter topping: ")))
+if toppings == "cheese":
+    toppings_price = int(0.50)
 
-    elif toppings == "peppers":
-        toppings_price = int(0.75)
+elif toppings == "peppers":
+    toppings_price = int(0.75)
 
-    elif toppings == "grilled onions":
-        toppings_price = int(1.00)
+elif toppings == "grilled onions":
+    toppings_price = int(1.00)
 
-    elif toppings == "none":
-        toppings_price = int(0.0)
+elif toppings == "none":
+    toppings_price = int(0.0)
 
 
 total_price = float(input((int(Order_Price) + int(toppings_price)) * 0.07))
