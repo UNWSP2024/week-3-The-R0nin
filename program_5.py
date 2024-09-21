adabf7f7e2edaa5214648a8f1b24b6f957863adb
The-R0nin <2025esteele@isd2144.org>
2024-09-20 21:39:39 -0500

    Update program_5.py
    Ethan Steele
    9/20

--- program_5.py
+++ program_5.py
@@ -1,29 +1,28 @@
-
 Order = input(str("Enter hotdog: "))
-#prices for the hotdogs variables \/
-Hotdog = int(3.50)
-
-Chilidog = int(4.50)
-#I am trying to trun the answer into a variable \/
-if Order == "hotdog" or "chilidog":
-    "hotdog" == Hotdog
-    "chilidog" ==  Chilidog
+Order_Price = 0.0
+if Order == "hotdog":
+    Order_Price = 3.50
     toppings = print(input(str("Enter topping: ")))
-#prices for the toppings variables\/
-Cheese = int(0.50)
 
-Peppers = int(0.75)
+elif Order == "chilidog":
+    Order_Price = 4.50
+    toppings = print(input(str("Enter topping: ")))
 
-Grilled_Onions = int(1.00)
+toppings_price = 0.0
+    if toppings == "cheese":
+        toppings_price = int(0.50)
 
-none = int(0.0)
-#I am trying to trun the answer into a variable \/
-if toppings == 'cheese' or 'peppers' or 'grilled onions':
-    'cheese' == Cheese
-    'peppers' == Peppers
-    'grilled onions' == Grilled_Onions
+    elif toppings == "peppers":
+        toppings_price = int(0.75)
 
-total_price = float(input((int(Order) + int(toppings)) * 0.07))
+    elif toppings == "grilled onions":
+        toppings_price = int(1.00)
+
+    elif toppings == "none":
+        toppings_price = int(0.0)
+
+
+total_price = float(input((int(Order_Price) + int(toppings_price)) * 0.07))
 
 print("Hotdog cost is: ", Order)
 print("The taxt is: 7%")
